c4bd3c0c5d6ba959b37ee6fe526617d0efbe4d70
José Rodrigues <josejunior170@hotmail.com>
2026-01-12 17:14:44 -0300

    feat: implementa fluxo de criação de postagens de texto

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from . import api
 
+
 urlpatterns = [
     path('me/', api.me, name='me'),
     path('signup/', api.signup, name='signup'),
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('api/', include('account.urls')),
+    path('api/posts/', include('post.urls')),
     path('admin/', admin.site.urls),
 ]
